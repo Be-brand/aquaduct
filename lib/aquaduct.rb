8cc87fbc0a2dbd4a10fd47e0c7d7c9e26b0e2c69
Eden Landau <edenworky@gmail.com>
2022-07-28 16:26:29 +0300

    refactor factory method out of the main lib file

--- lib/aquaduct.rb
+++ lib/aquaduct.rb
@@ -7,25 +7,4 @@ require_relative 'aquaduct/channels'
 require_relative 'aquaduct/channelable'
 require_relative 'aquaduct/channeler'
 require_relative 'aquaduct/persistence'
-
-module Aquaduct
-  def self.with_channels for_entity = :package, &channel_drawer
-    channels = Channels.draw &channel_drawer
-    Module.new do
-      const_set :CHANNELS, channels
-
-      entity_class_name = for_entity.to_s.camelize
-      entity_class = Class.new do
-        const_set :CHANNELS, channels
-        include Aquaduct::Channelable
-      end
-      const_set entity_class_name, entity_class
-
-      entity_channeler_class_name = "#{for_entity.to_s.camelize}Channeler"
-      entity_channeler_class = Class.new(Aquaduct::Channeler) do
-        const_set :CHANNELS, channels
-      end
-      const_set entity_channeler_class_name, entity_channeler_class
-    end
-  end
-end
+require_relative 'aquaduct/factory'
---
+++ lib/aquaduct/factory.rb
@@ -0,0 +1,22 @@
+module Aquaduct
+  def self.with_channels for_entity = :package, &channel_drawer
+    channels = Channels.draw &channel_drawer
+    Module.new do
+      const_set :CHANNELS, channels
+
+      entity_class_name = for_entity.to_s.camelize
+      entity_class = Class.new do
+        const_set :CHANNELS, channels
+        include Aquaduct::Channelable
+      end
+      const_set entity_class_name, entity_class
+
+      entity_channeler_class_name = "#{for_entity.to_s.camelize}Channeler"
+      entity_channeler_class = Class.new(Aquaduct::Channeler) do
+        const_set :CHANNELS, channels
+        include channels.persistence unless channels.persistence.nil?
+      end
+      const_set entity_channeler_class_name, entity_channeler_class
+    end
+  end
+end
